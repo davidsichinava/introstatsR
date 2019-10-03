75719339847c40d78c61b5ea9e7f4b25a6905931
David Sichinava <david@sichinava.ge>
2019-10-03 22:59:00 +0400

    please work

---
+++ pages/m1/class/class1.R
@@ -0,0 +1,4 @@
+setwd("D:\\Dropbox\\My projects\\Courses\\QT_Analysis\\website\\pages\\m1\\class")
+
+install.packages("plyr")
+
---
+++ pages/m1/lab_ibsu_1/lab1.R
@@ -0,0 +1,5 @@
+setwd("D:\\Dropbox\\My projects\\Courses\\QT_Analysis\\website\\pages\\m1\\lab_ibsu_1")
+
+install.packages("psych")
+
+library(psych)
